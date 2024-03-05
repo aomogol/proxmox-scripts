ee42fbacdba620c71cca6b2eaad2263bcb51bbe6
Ahmet Onder Mogol <aomogol@gmail.com>
2024-03-05 15:05:06 +0300

    add = ubuntu 24.04

--- ubuntu-cloud-24-04.sh
+++ ubuntu-cloud-24-04.sh
@@ -1,83 +1,111 @@
 #!/bin/bash
-    # Ubuntu Server 24.04 LTS (Noble Numbat) daily builds
-    # https://cloud-images.ubuntu.com/noble/current/noble-server-cloudimg-amd64.img
+
+# Ubuntu Server 24.04 LTS (Noble Numbat) daily builds
+# https://cloud-images.ubuntu.com/noble/current/noble-server-cloudimg-amd64.img
+
 set -e
-function echocolor() { # $1 = string
-    COLOR='\033[1;33m'
-    NC='\033[0m'
-    printf "${COLOR}$1${NC}\n"
-}
+
+# Set environment variables. Change these as necessary.
+export STORAGE_POOL="local"
+export VM_ID="8003"
+export VM_NAME="ubuntu-24.04-cloudimg"
+export VM_DISK_SIZE="8G"
+export VM_DISK_FORMAT="qcow2"
+export VM_DISK_PATH="/var/lib/libvirt/images"
+export VM_DISK_FILE="$VM_NAME.qcow2"
+export VM_MEMORY="1024"
+export VM_CPU="2"
+export VM_OS_TYPE="l26"
+export VM_OS_VARIANT="noble"
+export VM_OS_VERSION="24.04"
+export VM_OS_ARCH="amd64"
+export VM_OS_CLOUD_INIT_FILE="cloud-init.iso"
+export VM_OS_CLOUD_INIT_PATH="/var/lib/libvirt/images"
+export VM_OS_CLOUD_INIT_ISO_URL="https://cloud-images.ubuntu.com/noble/current/noble-server-cloudimg-amd64.img"
+export VM_OS_CLOUD_INIT_ISO_FILE="noble-server-cloudimg-amd64.img"
+export VM_CPU_TYPE="host"   
+export username="aom"
+export password="a"
 
 #Create template
-#args:
-# vm_id
-# vm_name
-# file name in the current directory
+
 function create_template() {
     #Print all of the configuration
-    echocolor "Creating template $2 ($1)"
-    echocolor "#Create new VM "
-        qm create $1 --name $2 --ostype l26 
-    echocolor "#Set networking to default bridge"
-        qm set $1 --net0 virtio,bridge=vmbr0
-    echocolor "#Set display to serial"
-        qm set $1 --serial0 socket --vga serial0
-    echocolor "#Set memory, cpu, type defaults"
+    echo "Creating template $VM_NAME  ($VM_ID)"
+    echo "#Create new VM "
+        qm create $VM_ID --name $VM_NAME --ostype $VM_OS_TYPE 
+#       qm create ${TEMPLATE_ID} --name "${TEMPLATE_IMAGE}-$(date +%Y%M%d)" --memory 2048 --cores 2 --net0 virtio,bridge=vmbr0
+    echo "#Set networking to default bridge"
+        qm set $VM_ID --net0 virtio,bridge=vmbr0
+    
+    echo "#Set display to serial"
+        qm set $VM_ID --serial0 socket --vga serial0
+    
+    echo "#Set memory, cpu, type defaults"
     #If you are in a cluster, you might need to change cpu type
-        qm set $1 --memory 1024 --cores 2 --cpu host
-    echocolor "#Set boot device to new file"
-        qm set $1 --scsi0 ${storage}:0,import-from="$(pwd)/$3",discard=on
-    echocolor "#Set scsi hardware as default boot disk using virtio scsi single"
-        qm set $1 --boot order=scsi0 --scsihw virtio-scsi-single
-    echocolor "#Enable Qemu guest agent in case the guest has it available"
-       qm set $1 --agent enabled=1,fstrim_cloned_disks=1
-    echocolor "#Add cloud-init device"
-        qm set $1 --ide2 ${storage}:cloudinit
-    echocolor "#Set CI ip config"
+        qm set $VM_ID --memory $VM_MEMORY --cores $VM_CPU --cpu $VM_CPU_TYPE
+    
+    echo "#Set boot device to new file"
+        qm set $VM_ID --scsi0 $STORAGE_POOL:0,import-from="$(pwd)/$VM_OS_CLOUD_INIT_ISO_FILE",discard=on
+    
+    echo "#Set scsi hardware as default boot disk using virtio scsi single"
+        qm set $VM_ID --boot order=scsi0 --scsihw virtio-scsi-single
+    
+    echo "#Enable Qemu guest agent in case the guest has it available"
+        qm set $VM_ID --agent enabled=1,fstrim_cloned_disks=1
+    
+    echo "#Add cloud-init device"
+        qm set $VM_ID --ide2 $STORAGE_POOL:cloudinit
+    
+    echo "#Set CI ip config"
     #IP6 = auto means SLAAC (a reliable default with no bad effects on non-IPv6 networks)
     #IP = DHCP means what it says, so leave that out entirely on non-IPv4 networks to avoid DHCP delays
-    #qm set $1 --ipconfig0 "ip6=auto,ip=dhcp"
-        qm set $1 --ipconfig0 "ip=dhcp"
+    #qm set $VM_ID --ipconfig0 "ip6=auto,ip=dhcp"
+        qm set $VM_ID --ipconfig0 "ip=dhcp"
+    
     #Import the ssh keyfile
-        #qm set $1 --sshkeys ${ssh_keyfile}
-    echocolor "#Add the User & Password"
-        qm set $1 --ciuser ${username}
+        #qm set $VM_ID --sshkeys ${ssh_keyfile}
+    
+    echo "#Add the User & Password"
+        qm set $VM_ID --ciuser $username
     #If you want to do password-based auth instaed
     #Then use this option and comment out the line above
-        qm set $1 --cipassword ${password}
-    echocolor "#Resize the disk to 8G, a reasonable minimum. You can expand it more later."
+        qm set $VM_ID --cipassword $password
+    
+    echo "#Resize the disk."
     #If the disk is already bigger than 8G, this will fail, and that is okay.
-        qm disk resize $1 scsi0 8G
-    echocolor "#Make it a template"
-        qm template $1
-    echocolor "#Remove file when done"
-        rm $3
+        qm disk resize $VM_ID scsi0 $VM_DISK_SIZE
+    
+    echo "#Make it a template"
+        qm template $VM_ID
+    
+    echo "#Remove file when done"
+        rm $VM_OS_CLOUD_INIT_ISO_FILE
 }
 
 #Path to your ssh authorized_keys file
 #Alternatively, use /etc/pve/priv/authorized_keys if you are already authorized on the Proxmox system
 #export ssh_keyfile=/root/id_rsa.pub
 
-#Username to create on VM template
-export username=aom
-export password=a
+imagefile=/root/download/$VM_OS_CLOUD_INIT_ISO_FILE
 
-#Name of your storage
-export storage=local
-
-# Ubuntu Server 24.04 LTS (Noble Numbat) daily builds
-# https://cloud-images.ubuntu.com/noble/current/noble-server-cloudimg-amd64.img
-
-imagefile=/root/download/noble-server-cloudimg-amd64.img
 if test -f "$imagefile"; then
-     echo "found img file skipping download..."
+    echo "found img file skipping download..."
 else
-     echocolor "downloading img file..."
-     cd /root/download
-     wget "https://cloud-images.ubuntu.com/noble/current/noble-server-cloudimg-amd64.img"   
+    echo "downloading img file..."
+    cd /root/download
+    wget $VM_OS_CLOUD_INIT_ISO_URL
 fi
 
+# Install apps on Ubuntu image.
 
-create_template 8001 "temp-ubuntu-24-04" "noble-server-cloudimg-amd64.img"
+    ###      --firstboot-install PKG,PKG..
 
-echocolor " işlemler tamamlandı."
+virt-customize -a $VM_OS_CLOUD_INIT_ISO_FILE --install qemu-guest-agent,neofetch,git,bash-completion
+# Enable password authentication in the template. Obviously, not recommended for except for testing.
+virt-customize -a $VM_OS_CLOUD_INIT_ISO_FILE --run-command "sed -i 's/.*PasswordAuthentication.*/PasswordAuthentication yes/g' /etc/ssh/sshd_config"
+virt-customize -a $VM_OS_CLOUD_INIT_ISO_FILE --run-command "sed -i 's/.*PasswordAuthentication.*/PasswordAuthentication yes/g' /etc/ssh/sshd_config.d/60-cloudimg-settings.conf"
+
+create_template $VM_ID $VM_NAME $VM_OS_CLOUD_INIT_ISO_FILE
+
+echo " işlemler tamamlandı."
